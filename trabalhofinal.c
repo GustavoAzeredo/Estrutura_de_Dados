b2468db323ff34a5ffd35c7ed52818f5a1f95238
Gustavo Azeredo <gustavoazeredo@gmail.com>
2021-06-28 18:40:16 -0300

    parte 1

--- trabalhofinal.c
+++ trabalhofinal.c
@@ -12,20 +12,30 @@ typedef struct lista{
 
 char PegarNome(char str[])
 {
+	int tn = 0;
 	setbuf(stdin,NULL);
 
-		printf("Digite o nome de Título:\n");
+		printf("Digite o nome de Título: ");
 			fgets(str,6,stdin);
+				tn = strlen(str);
+				if (str[tn-1]=='\n')
+					str[tn-1]='\0';
 
 	return str[6];
 }
 
 char PegarTipo(char str[])
 {
+	int tn = 0;
 	setbuf(stdin,NULL);
 
-		printf("Diga o tipo, (comp) para compra e (vend) para venda:\n");
+		printf("Diga o tipo, (comp) para compra e (vend) para venda: ");
 			fgets(str,5,stdin);
+
+				tn = strlen(str);
+				if (str[tn-1]=='\n')
+					str[tn-1]='\0';
+
 
 	return str[5];
 }
@@ -36,10 +46,9 @@ void BuscarPorNome(Lista* l1, char str[])
 
 	for (busca = l1; busca != NULL; busca = busca->prox){
 		if (!strcmp(str,busca->nome)){
-			printf("nome: %s\ntipo: %s\nvalor: %f\nquantidade:%d\n",busca->nome,busca->tipo,busca->valor,busca->quantidade);
+			printf("\nnome: %s\ntipo: %s\nvalor: %.2f\nquantidade:%d\n\n",busca->nome,busca->tipo,busca->valor,busca->quantidade);
 		}
-		else
-			printf("Não existe esse título!\n");
+		
 	}
 }
 
@@ -48,11 +57,10 @@ void BuscarPorTipo(Lista* l1, char str[])
 	Lista* busca;
 
 	for (busca = l1; busca != NULL; busca = busca->prox){
-		if (!strcmp(str,busca->nome)){
-			printf("nome: %s\ntipo: %s\nvalor: %f\nquantidade:%d\n",busca->nome,busca->tipo,busca->valor,busca->quantidade);
+		if (!strcmp(str,busca->tipo)){
+			printf("\nnome: %s\ntipo: %s\nvalor: %.2f\nquantidade:%d\n\n",busca->nome,busca->tipo,busca->valor,busca->quantidade);
 		}
-		else
-			printf("Não existe esse tipo!\n");
+		
 	}
 }
 
@@ -68,41 +76,43 @@ int main()
 	int tb = 0;
 
 	l1 = (Lista*)malloc(sizeof(Lista));
-	strcpy(l1->nome,n1);
-	strcpy(l1->tipo,t1);
+	strncpy(l1->nome,n1,5);
+	strncpy(l1->tipo,t1,4);
 	l1->valor = 25.50;
 	l1->quantidade = 200;
 
-		l2 = (Lista*)malloc(sizeof(Lista));
-		strcpy(l1->nome,n2);
-		strcpy(l1->tipo,t2);
-		l2->valor = 26.00;
-		l2->quantidade = 250;
+	l2 = (Lista*)malloc(sizeof(Lista));
+	strncpy(l2->nome,n2,5);
+	strncpy(l2->tipo,t2,4);
+	l2->valor = 26.00;
+	l2->quantidade = 250;
 
-			l3 = (Lista*)malloc(sizeof(Lista));
-			strcpy(l1->nome,n3);
-			strcpy(l1->tipo,t2);
-			l3->valor = 26.50;
-			l3->quantidade = 300;
+	l3 = (Lista*)malloc(sizeof(Lista));
+	strncpy(l3->nome,n3,5);
+	strncpy(l3->tipo,t2,4);
+	l3->valor = 26.50;
+	l3->quantidade = 300;
 
-				l1->prox = l2;
-				l2->prox = l3;
-				l3->prox = NULL;
+	l1->prox = l2;
+	l2->prox = l3;
+	l3->prox = NULL;
 
-				printf("Digite 1: Para buscar por nome\n");
-				printf("Digite 2: Para buscar por tipo\n");
-					scanf("%d",&tb);
+		printf("Digite 1: Para buscar por nome\n");
+		printf("Digite 2: Para buscar por tipo\n");
+		printf("\n");
+		scanf("%d",&tb);
+		printf("\n");
 
-						if (tb==1){
-							PegarNome(string1);
-							BuscarPorNome(l1, string1);
-						}
-						else if (tb==2){
-							PegarTipo(string2);
-							BuscarPorTipo(l1, string2);
-						}
-						else
-							printf("Opção Inválida!\n");
+			if (tb==1){
+				PegarNome(string1);
+				BuscarPorNome(l1, string1);
+			}
+			else if (tb==2){
+				PegarTipo(string2);
+				BuscarPorTipo(l1, string2);
+			}
+			else
+				printf("Opção Inválida!\n");
 
 				
 	return 0;
